74f0fd49c889e16a82eb4ab87a178c863481efdf
Yorfad <alexmej267@gmail.com>
2025-08-29 23:20:57 -0600

    otra vez

--- react-ts/src/main.jsx
+++ react-ts/src/main.jsx
@@ -1,7 +1,7 @@
 // main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 import { ProtectedRoute } from "./auth/ProtectedRoute";
 import App from "./App.jsx";
